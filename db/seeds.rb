6aa64b293f4cf99ff627968b1e79a88f60378ed4
christian salako <christian.s5547@gmail.com>
2020-03-03 16:08:23 +0000

    cloudinary and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# require "open-uri"
 
 
-puts 'Cleaning database'
-Product.destroy_all
-puts 'database is clean'
+# file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
 
 
-puts 'Creating users'
-10.times do
-  user = User.create!(
-    email: Faker::Internet.email,
-    password: "hello123"
-  )
-  puts "users #{user.id} is created"
-end
+# puts 'Cleaning database'
+# Product.destroy_all
+# puts 'database is clean'
 
-puts 'done'
+
+# puts 'Creating users'
+# 10.times do
+#   user = User.create!(
+#     email: Faker::Internet.email,
+#     password: "hello123"
+#   )
+#   puts "users #{user.id} is created"
+# end
+
+# puts 'done'
 
 
 
-puts 'Creating products'
-10.times do
-    product = Product.create!(
-    name: Faker::Commerce.department,
-    category: Faker::Commerce.department,
-    user_id: rand(1..10)
-  )
-  puts "product #{product.id} is created"
-end
+# puts 'Creating products'
+# 10.times do
+#     product = Product.create!(
+#       name: Faker::Commerce.department,
+#       category: Faker::Commerce.department,
+#       user_id: rand(1..10),
+#     )
+#   file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+#     product.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+#   puts "product #{product.id} is created"
+# end
 
-puts 'done'
+# puts 'done'
+
+
+
+
+
+
+
+
