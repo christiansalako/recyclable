9989f20e7ecacb2aebe5f1cc8a3d47b0b62467d5
christian salako <christian.s5547@gmail.com>
2020-03-04 14:36:47 +0000

    added link on producer page to create new product

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_03_03_121126) do
-=======
 ActiveRecord::Schema.define(version: 2020_03_03_174335) do
->>>>>>> 7785dbbb362abbda54b9afb119fe9b14f3913611
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,6 +38,61 @@
 
 
 
+# puts 'Creating products'
+# 10.times do
+#     product = Product.create!(
+#       name: Faker::Commerce.department,
+#       category: Faker::Commerce.department,
+#       user_id: rand(1..10),
+
+#   file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+#     product.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+#   puts "product #{product.id} is created"
+# end
+
+# puts 'done'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+puts 'Cleaning database'
+Product.destroy_all
+puts 'database is clean'
+
+
+puts 'Creating users'
+3.times do
+  Material.create!(
+  category:[i] ["glass", "wood", "plastic"]
+  recyclability:[i] [true, false, true]
+   )
+   puts "materials is created"
+ end
+
+ puts 'done'
+
+
+
+
+
+
+
+
+
+
+
 
 
 
