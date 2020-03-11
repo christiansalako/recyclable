49e094b12929879088e3d4d67372d16940808f67
christian salako <christian.s5547@gmail.com>
2020-03-11 12:02:16 +0000

    game complete css required

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,37 +5,63 @@ require("channels")
 
 import "bootstrap";
 
-const myButton = document.getElementById('green-answer')
-const secondButton = document.getElementById('secondtest')
 
-console.log('myButton info', myButton.dataset.recyclability);
 
-myButton.addEventListener('click', function() {
-  if (myButton.dataset.recyclability === 100) {
-    Swal.fire ({
-      icon: 'Good job!',
-      title: 'You clicked the button!',
-      text: 'success'
+document.addEventListener('turbolinks:load', () => {
+  $('.carousel').carousel({
+      interval: false
+  });
+
+  const greenButtons = document.querySelectorAll('.green-answer')
+  const secondButton = document.querySelectorAll('.red-answer')
+  greenButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      if (button.dataset.recyclability == 100) {
+        Swal.fire ({
+          icon: 'success',
+          title: 'You go that right!',
+          text: 'success'
+        })
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'You got that one wrong!',
+        })
+      }
     })
-  } else {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'You got that one wrong!',
+  })
+  secondButton.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      if (button.dataset.recyclability == 0) {
+        Swal.fire ({
+          icon: 'Good job!',
+          title: 'You clicked the button!',
+          text: 'success'
+        })
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'You got that one wrong!',
+        })
+      }
     })
-  }
-});
-
-
-secondButton.addEventListener('click', function() {
-  Swal.fire({
-  icon: 'error',
-  title: 'Oops...',
-  text: 'You got that one wrong!',
-
+  })
 })
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
